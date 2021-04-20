1494456a4004b3c16157971541248a18fa4fa50c
arjunamayakara <52194004+arjunamayakara@users.noreply.github.com>
2021-04-20 09:57:47 +0530

    Update logger.groovy

--- vars/logger.groovy
+++ vars/logger.groovy
@@ -2,16 +2,14 @@ def logger(String str, String level)
 {
    if (level == "ALERT")
    {
-        ansiColor('xterm')
-        {
+        
             echo '\033[32m' + str // Green
-        }
+        
    }
    else if (level == "BOLD")
    {
-        ansiColor('xterm')
-        {
+       
            echo '\033[1;31m' + str // Green
-        }
+        
    }
 }
